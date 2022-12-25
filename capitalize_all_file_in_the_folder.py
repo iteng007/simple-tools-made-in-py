ce2075dec3bfc13aaa9ba573ea30f73ae2d4de40
iteng007 <iteng0718@gmail.com>
2022-12-26 00:05:10 +0800

    新文件：   capitalize_all_file_in_the_folder.py

---
+++ capitalize_all_file_in_the_folder.py
@@ -0,0 +1,19 @@
+import os
+import sys
+if len(sys.argv)!=2:
+    print("Wrong usage,please enter a folder name")
+    sys.exit(1)
+
+
+dirs = []
+
+with os.scandir(sys.argv[1]) as entries:
+    for entry in entries:
+        if entry.name != sys.argv[0] and entry.is_file():
+            dirs.append(entry.name)
+
+new_files_names = []
+for each in dirs:
+    tmp = each.capitalize()
+    os.rename(each,tmp)
+
